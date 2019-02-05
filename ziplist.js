5201f8a188e79808c424a0455b2437fce7ed8916
nicholasmp <nkmp@hawaii.edu>
2019-02-04 17:55:32 -1000

    Finished E27 WOD

---
+++ ziplist.js
@@ -0,0 +1,19 @@
+/*
+*First Try: 25:00+ DNF
+* Second Try: 14:58 RX
+ */
+const alpha = ['a', 'b', 'c'];
+const num = [1, 2, 3];
+
+function zipList(list1, list2){
+  let list = [];
+  for(let i =0; i<list1.length;i++){
+    list.push(list1[i],list2[i]);
+  }
+  return list;
+}
+console.log(zipList(alpha, num));
+function zipListTheEasyWay(list1, list2){
+  return _.flatten(_.zip(list1,list2));
+}
+console.log(zipListTheEasyWay(alpha, num));
