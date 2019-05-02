433039ec44648c04f167e53d7c13cd8100a05ba6
nicholasmp <nkmp@hawaii.edu>
2019-05-01 20:31:03 -1000

    Pre E61 Fixes

--- ziplist.js
+++ ziplist.js
@@ -5,15 +5,15 @@
 const alpha = ['a', 'b', 'c'];
 const num = [1, 2, 3];
 
-function zipList(list1, list2){
-  let list = [];
-  for(let i =0; i<list1.length;i++){
-    list.push(list1[i],list2[i]);
+function zipList(list1, list2) {
+  const list = [];
+  for (let i = 0; i < list1.length; i++) {
+    list.push(list1[i], list2[i]);
   }
   return list;
 }
 console.log(zipList(alpha, num));
-function zipListTheEasyWay(list1, list2){
-  return _.flatten(_.zip(list1,list2));
+function zipListTheEasyWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
 }
 console.log(zipListTheEasyWay(alpha, num));
